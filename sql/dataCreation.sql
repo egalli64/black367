aa9186f8dafe3071e0f83699834989686a328f17
ariannaniccolo <ariannaniccolo@live.it>
2020-09-10 16:21:03 +0200

    adding new columns

--- sql/dataCreation.sql
+++ sql/dataCreation.sql
@@ -9,14 +9,17 @@ create table users(
     user_password varchar(20) not null,
     first_name varchar(20),
     last_name varchar(25),
-    email varchar(25)
+    email varchar(25),
+    birth_date date,
+    gender varchar(1),
+    city varchar(20)
 );
-insert into users (user_id, user_name, user_password, first_name, last_name, email) values
-('1','Federico', 'Federico11', 'Federico', 'Medini', 'federico@gmail.com'),
-('2','Arianna', 'Arianna22', 'Arianna', 'Niccolo', 'arianna@gmail.com'),
-('3','Giulia', 'Giulia33', 'Giulia', 'Pontiggia', 'giulia@gmail.com'),
-('4','Luigi', 'Luigi44', 'Luigi', 'De Berardinis', 'luigi@gmail.com'),
-('5','Nicole', 'Nicole55', 'Nicole', 'Discepoli', 'nicole@gmail.com'),
-('6','MariaGrazia', 'Mariag66', 'Maria Grazia', 'Restifo', 'mariagrazia@gmail.com');
+insert into users (user_id, user_name, user_password, first_name, last_name, email, birth_date, gender, city) values
+('1','Federico', 'Federico11', 'Federico', 'Medini', 'federico@gmail.com','1993-11-11','M','Bologna'),
+('2','Arianna', 'Arianna22', 'Arianna', 'Niccolo', 'arianna@gmail.com','1994-03-03','F','Mantova'),
+('3','Giulia', 'Giulia33', 'Giulia', 'Pontiggia', 'giulia@gmail.com','1995-02-22','F','Lodi'),
+('4','Luigi', 'Luigi44', 'Luigi', 'De Berardinis', 'luigi@gmail.com','1995-12-12','M','Bari'),
+('5','Nicole', 'Nicole55', 'Nicole', 'Discepoli', 'nicole@gmail.com','1995-07-25','F','Ancona'),
+('6','MariaGrazia', 'Mariag66', 'Maria Grazia', 'Restifo', 'mariagrazia@gmail.com','1995-10-18','F','Palermo');
 
 commit;

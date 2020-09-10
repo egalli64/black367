79b5558ecf437f1e541d470d838a512a3a35515c
Federico Medini <medo.federico@gmail.com>
2020-09-10 12:21:18 +0200

    Servlet Login checking on database for user

--- src/main/java/srv/Login.java
+++ src/main/java/srv/Login.java
@@ -1,6 +1,9 @@
 package srv;
 
 import java.io.IOException;
+import java.time.LocalTime;
+
+import javax.annotation.Resource;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -8,6 +11,10 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  * Servlet implementation class Login
@@ -15,29 +22,39 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-	
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	private static final Logger LOG = LoggerFactory.getLogger(Login.class);
+
+	@Resource(name = "jdbc/me")
+	private DataSource ds;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String user = request.getParameter("user");
 		String password = request.getParameter("password");
-		request.setAttribute("user", user);
-//		request.setAttribute("password", password);
-		
-		HttpSession session = request.getSession();
-		session.setAttribute("user", user);
-		session.setAttribute("password", password);
-		
-//		LocalTime start = (LocalTime) session.getAttribute("start");
 
-		if ((user != null) && (!user.isEmpty()) && (password != null) && (!password.isEmpty())) {
-			RequestDispatcher rd = request.getRequestDispatcher("/index_bis.jsp");
-			rd.forward(request, response);
+		try (UserDAO dao = new UserDAO(ds)) {
+			request.setAttribute("user", dao.getAll());
+
+			if (dao.verify(user, password) != null) {
+				LOG.info("User " + user + " IS in the database");
+				request.setAttribute("user", dao.verify(user,password));
+				HttpSession session = request.getSession();
+				session.setAttribute("user", user);
+				session.setAttribute("logged", true);
+				LocalTime start = (LocalTime) session.getAttribute("start");
+				RequestDispatcher rd = request.getRequestDispatcher("/index_bis.jsp");
+				rd.forward(request, response);
+			} else {
+				LOG.info("User " + user + " is NOT in the database");
+				RequestDispatcher rd = request.getRequestDispatcher("/login.jsp");
+				rd.forward(request, response);
+			}
 		}
 	}
 
-
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 

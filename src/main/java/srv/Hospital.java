4014014976f0137464ac892a91c4ac36b8019893
ariannaniccolo <ariannaniccolo@live.it>
2020-09-17 17:01:30 +0200

    JavaBean ospedali

---
+++ src/main/java/srv/Hospital.java
@@ -0,0 +1,44 @@
+package srv;
+
+public class Hospital {
+	private int id;
+	private String name;
+	private String address;
+	private int cap;
+	private String city;
+	public int getId() {
+		return id;
+	}
+	
+	
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public int getCap() {
+		return cap;
+	}
+	public void setCap(int cap) {
+		this.cap = cap;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	
+	
+}
